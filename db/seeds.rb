bb73be3884c9993639db71dcab93f45dbfb50519
sofia ngapani <withsofina@gmail.com>
2022-08-18 16:51:40 +0800

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Restaurant.description,
+    phone_number: Faker::Restaurant.type,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+
+  restaurant.save!
+end
